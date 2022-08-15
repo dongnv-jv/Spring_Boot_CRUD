20251fee5c24ac6d66588596566d43681a48d8bd
dongbk97 <dongtr2202@gmail.com>
2022-08-15 09:03:33 +0700

    update Edit method v2

---
+++ Spring_Boot_CRUD/src/test/java/com/example/spring_boot_crud/TestUsserRepo.java
@@ -0,0 +1,15 @@
+package com.example.spring_boot_crud;
+//import static org.aspectj.core.api.Assertion;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+@SpringBootTest
+public class TestUsserRepo {
+    @Test
+    public void addUser() {
+
+    }
+}
